f7d0e805248ce5fa1bf3f9baf259cae4d59a0e79
ritizzzz <kandelritiz@gmail.com>
2022-06-16 12:55:24 +1000

    email validation added

--- formwithvalidation/script.js
+++ formwithvalidation/script.js
@@ -3,8 +3,11 @@ const patterns = {
     telephone: /^0\d{10}$/,
     username: /^[a-z\d]{5,12}$/i,
     password: /^[\w@-]{8,20}$/,
-    slug: /^[a-z\d-]{8,20}$/
+    slug: /^[a-z\d-]{8,20}$/,
+    // https://www.youtube.com/watch?v=QxjAOSUQjP0&list=PL4cUxeGkcC9g6m_6Sld9Q4jzqdqHd2HiD&index=15
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
+
 
 function validate(field, regex){
     console.log(regex.test(field.value));
