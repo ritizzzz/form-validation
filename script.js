84b029ae483a1da402965ea5d52a2ce396d6ec0a
ritizzzz <kandelritiz@gmail.com>
2022-06-16 10:07:29 +1000

    regex character sets, refer to changes

--- script.js
+++ script.js
@@ -4,3 +4,20 @@
 // every match
 const string = 'ninja asdfas Ninja';
 console.log(string.match(/ninja/gi));
+
+// patterns
+// character sets is way to match different chracters in a single position
+const string1 = 'ninja ginja adfadf';
+
+//match either n or g in the first position
+// chracter sets don't need something after them
+console.log(string1.match(/[ng]inja/gi))
+
+//match all instance of a and f and h
+console.log(string1.match(/[afh]/g)) 
+
+// exclude sets
+const string2 = 'a000 b000 c000 p000 %000'
+
+// match all except p000 and a000 exclude(^)
+console.log(string2.match(/[^pa]000/g))
